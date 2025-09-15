0989391238a21b6e4ba9ea12aab38d76792f8b12
testing-sudo <kajalthakar2809@gmail.com>
2025-09-15 15:40:34 +0100

    new changes

--- SOTank/SOTank.java
+++ SOTank/SOTank.java
@@ -27,11 +27,9 @@ public class SOTank extends Bot {
     public void run() {
         // Repeat while the bot is running
         while (isRunning()) {
-            forward(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-
+            turnGunRight(270);
+            back(200);
+            turnGunRight(270);
 
         }
     }
@@ -39,7 +37,7 @@ public class SOTank extends Bot {
     // We saw another bot -> fire!
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-        fire(5);
+        fire(8);
     }
 
     // We were hit by a bullet -> turn perpendicular to the bullet
